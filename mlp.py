1edc9c8b8334c74f65997171609da4a33d5fab93
Nick Cai <nickcai@pop-os.localdomain>
2021-11-30 17:56:37 -0800

    Updated neural network implementaiton and added K-folds cross validation.

--- mlp.py
+++ mlp.py
@@ -1,8 +1,10 @@
+import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 
+
 class MLP(nn.Module):
-    def __init__(self, input_size: int, output_size: int, hidden_sizes: list, device='cuda'):
+    def __init__(self, input_size: int, output_size: int, hidden_sizes: list, act: str, device='cuda'):
         super(MLP, self).__init__()
         num_hidden = len(hidden_sizes)
         assert(num_hidden >= 1)
@@ -13,12 +15,17 @@ class MLP(nn.Module):
                 self.fc.extend([nn.Linear(hidden_sizes[i - 1], hidden_sizes[i], device=device)])
                 self.fc.extend([nn.Dropout(p=0.5)])
         self.fc.extend([nn.Linear(hidden_sizes[-1], output_size, device=device)])
-        self.relu = F.relu
+        if act == 'relu':
+            self.act = F.relu   
+        elif act == 'tanh':
+            self.act = torch.tanh
+        else:
+            assert False
 
     def forward(self, x):
         for i, fc in enumerate(self.fc):
             x = fc(x)
             if i < len(self.fc) - 1:
-                x = self.relu(x)
+                x = self.act(x)
 
         return x
