fcd05941a149cd4263d5fab6939105b4a17a75c3
GuangyanCai <g5cai@ucsd.edu>
2021-12-09 21:20:08 -0800

    Added a script to plot all roc curves.

---
+++ plot_roc_curves.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.metrics import roc_curve
+from os.path import join 
+from matplotlib import pyplot as plt
+
+dir_pred = 'prediction'
+svm_y_test = np.loadtxt(join(dir_pred, 'svm_y_test.txt'))
+svm_yhat_test = np.loadtxt(join(dir_pred, 'svm_yhat_test.txt'))
+mlp_y_test = np.loadtxt(join(dir_pred, 'mlp_y_test.txt'))
+mlp_yhat_test = np.loadtxt(join(dir_pred, 'mlp_yhat_test.txt'))
+rf_y_test = np.loadtxt(join(dir_pred, 'rf_y_test.txt'))
+rf_yhat_test = np.loadtxt(join(dir_pred, 'rf_yhat_test.txt'))
+
+svm_fpr, svm_tpr, _ = roc_curve(svm_y_test, svm_yhat_test)
+mlp_fpr, mlp_tpr, _ = roc_curve(mlp_y_test, mlp_yhat_test)
+rf_fpr, rf_tpr, _ = roc_curve(rf_y_test, rf_yhat_test)
+
+plt.plot(svm_fpr, svm_tpr, label='svm')
+plt.plot(mlp_fpr, mlp_tpr, label='mlp')
+plt.plot(rf_fpr, rf_tpr, label='rf')
+plt.legend(title='classifier')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curves of Our Classifiers')
+plt.savefig('roc_plot.png')
